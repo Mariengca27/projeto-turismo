28636a3248091e2bafd7d9579bf9618bd75d8274
Mariengca27 <mariengca27>
2024-03-11 13:59:53 -0300

    Outras rotas POST,PUT,EDIT,CREATE,DELETE E GET

--- app/Http/Controllers/PontosTuristicosController.php
+++ app/Http/Controllers/PontosTuristicosController.php
@@ -16,5 +16,51 @@ return view("turisticos.index");
     
 }
 
+//Detalhes dos pontos turísticos
+public function show(){
+
+  
+    
+    return view("turisticos.show");
+    
+        
+    }
+
+//Carregar o formulário para cadastgro das sugestões de novos pontos turísticos
+public function create(){
+
+  
+    
+    return view("turisticos.create");
+    
+        
+    }
+//Cadastra no banco de dados um novo ponto turístico
+    public function store(){
+            dd("");
+        }
+    
+
+
+//Carregar página para editar pontos turísticos existentes
+public function edit(){ 
+        
+    return view("turisticos.edit");
+    }
+
+//Carregar página para atualizar pontos turísticos existentes no banco de dados
+public function update(){ 
+        dd("Atualizar no banco de dad");
+}
+
+
+//Carregar página para deletar pontos turísticos existentes
+public function destroy(){ 
+        dd("Deletar dados no BD");
+}
+
+
+
+
 
 }
--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,9 @@ Route::get('/', function () {
 //Rotas para pegar os pontos turísticos:
 
 Route::get('/index-turismo',[PontosTuristicosController::class,'index']);
-
+Route::get('/show-detalhes-turismo',[PontosTuristicosController::class,'show']);
+Route::get('/create-turismo',[PontosTuristicosController::class,'create']);
+Route::post('/store-novo-registro-turismo',[PontosTuristicosController::class,'store']);
+Route::get('/edit-detalhes-turismo',[PontosTuristicosController::class,'edit']);
+Route::put('/put-detalhes-turismo',[PontosTuristicosController::class,'update']);
+Route::delete('/delete-turismo',[PontosTuristicosController::class,'destroy']);
