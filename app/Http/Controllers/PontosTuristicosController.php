b305c059f51088683e25e086c75d6d9ef3d28b56
Mariengca27 <mariengca27>
2024-03-11 07:33:41 -0300

    Primeiro Controller e primeira rota de busca pontos turisticos

---
+++ app/Http/Controllers/PontosTuristicosController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PontosTuristicosController extends Controller
+{
+    
+public function index(){
+
+
+
+
+    
+}
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\PontosTuristicosController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Rotas para pegar os pontos turísticos:
+
+Route::get('/index-turismo',[PontosTuristicosController::class,'index']);
+
